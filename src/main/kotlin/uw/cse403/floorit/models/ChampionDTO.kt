9aa900e3f63b3bf24fed26f230348ecb574b9aae
Andrew Nguyen <andrewn1@cs.washington.edu>
2025-02-03 18:50:54 -0800

    changes

--- src/main/kotlin/uw/cse403/floorit/models/ChampionDTO.kt
+++ src/main/kotlin/uw/cse403/floorit/models/ChampionDTO.kt
@@ -7,3 +7,6 @@ data class Champion(val name: String, val title: String)
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class ChampionResponse(val data: Map<String, Champion>)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class ChampionResponseData(val data: Map<String, Champion>)
