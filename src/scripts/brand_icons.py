467630081bc4ed7dac467d2ced50c70c5529a358
Andrew Nguyen <andrewn1@cs.washington.edu>
2025-02-06 13:14:18 -0800

    added brand category

--- src/scripts/brand_icons.py
+++ src/scripts/brand_icons.py
@@ -1,6 +1,12 @@
+from src.models.brand_icon_dto import BrandIconsDTO
 from src.utils.category import Category
 
 
-class BrandIcons(Category):
-    img: str
-    names: str
+class BrandIcons(Category[BrandIconsDTO]):
+    def __init__(self, file: str):
+        super().__init__(file, BrandIconsDTO)
+
+
+if __name__ == "__main__":
+    icons = BrandIcons("brand-icons.json")
+    print(icons.data)
