8e58749d3e93b9322d6964adb0a99014595b251a
Andrew Nguyen <andrewn1@cs.washington.edu>
2025-02-06 17:30:22 -0800

    testing

--- src/utils/category_mapper.py
+++ src/utils/category_mapper.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from src.models.category_data_dto import (
     CategoryDataDTO,
     QuestionType,
@@ -6,23 +8,27 @@ from src.models.category_data_dto import (
 
 
 def to_category_data(
-    category_name: str, question_type: QuestionType, json_data: dict
+    category_name: str, question_type: QuestionType, json_data: dict[str, List[str]]
 ) -> CategoryDataDTO:
     """
-    This maps JSON data {<question>: <answer>, <question>: <answer>} into a CategoryDataDTO.
+    Maps JSON data {<question>: [<answer>], <question>: [<answer>]} into a CategoryDataDTO.
 
     :param category_name: the category name
     :param question_type: the QuestionType of the category
     :param json_data: the processed category data
     :return: CategoryDataDTO of the category
     """
+    if not isinstance(json_data, dict):
+        raise ValueError("json_data should be a dict")
 
-    trivia_questions = []
     for q, a in json_data.items():
-        tq = TriviaQuestionDTO(question=q, answers=a)
-
-        trivia_questions.append(tq)
+        if not isinstance(a, list):
+            raise ValueError("answers should be a list of strings")
 
     return CategoryDataDTO(
-        name=category_name, type=question_type, questions=trivia_questions
+        name=category_name,
+        type=question_type,
+        questions=[
+            TriviaQuestionDTO(question=q, answers=a) for q, a in json_data.items()
+        ],
     )
