7bcb2a5c9866dd5b9a8575f6e6c9ddee36390bfd
Andrew Nguyen <andrewn1@cs.washington.edu>
2025-02-06 18:53:55 -0800

    started testing

--- src/scripts/brand_icons.py
+++ src/scripts/brand_icons.py
@@ -5,3 +5,6 @@ from src.utils.category import Category
 class BrandIcons(Category[BrandIconsDTO]):
     def __init__(self, file: str):
         super().__init__(file, BrandIconsDTO)
+
+    def brand_logo_to_name(self) -> dict:
+        return {q.partial: q.answers for q in self.data.questions}
