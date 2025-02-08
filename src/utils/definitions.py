101933c4d7b186d0fe7690e01d17fa4c5e9ca558
Andrew Nguyen <59133789+apnguyen1@users.noreply.github.com>
2025-02-08 00:11:59 -0800

    (category): Brand-Icon Category - ticket-56 (#57)
    * images
    
    * add json
    
    * removed matching script
    
    * adding json
    
    * adding json
    
    * adding constant definitions
    
    * adding constant definitions
    
    * added brand category
    
    * removed unnecessary file sizes
    
    * started testing
    
    * finished testing
    
    ---------
    
    Co-authored-by: Andrew Nguyen <andrewn1@cs.washington.edu>

---
+++ src/utils/definitions.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent.parent  # ROOT_DIR
+
+STATIC_RESOURCE_DIR = ROOT_DIR / "src/resources/static"  # static files
--- src/utils/parse_file.py
+++ src/utils/parse_file.py
@@ -1,19 +1,27 @@
+import json
 from pathlib import Path
 
+from src.utils.definitions import STATIC_RESOURCE_DIR
 
-def parse_file(filename):
+
+def parse_file(filename) -> dict:
     """
     Parse a file and return a dictionary of the parsed data.
 
     :param filename: The file to be parsed.
     :return: # TODO
     """
-    file_path = Path(
-        f"src/resources/static/{filename}"
-    )  # TODO: remove the string literal and replace with constant
+    file_path = Path(STATIC_RESOURCE_DIR / filename)
+
     if not file_path.exists():
-        err_msg = f"File path: {file_path} does not exist"
-        raise FileNotFoundError(err_msg)
-    with open(file_path, "r") as file:
-        print(file)
+        raise FileNotFoundError(f"File {filename} does not exist.")
+
+    if file_path.suffix == ".json":
+        with open(file_path, "r") as file:
+            data = json.load(file)
+    elif file_path.suffix == ".csv":
         pass  # TODO
+    elif file_path.suffix == ".txt":
+        pass  # TODO
+
+    return data
