5577aae77f446110ffcd29ce2e2f0dc13f366642
Andrew Nguyen <andrewn1@cs.washington.edu>
2025-02-08 03:52:55 -0800

    errors

--- frontend/src/hooks/useGame.ts
+++ frontend/src/hooks/useGame.ts
@@ -2,10 +2,7 @@ import { useContext } from 'react';
 import { GameContext } from '../context/GameContext.tsx';
 
 export const useGame = () => {
-  const context =
-    
-    
-    useContext(GameContext);
+  const context = useContext(GameContext);
   if (!context) throw new Error('useGame must be used to use');
 
   return context;
