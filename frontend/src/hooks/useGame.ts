c2fc69249cf8987e35112d7668adae31e7e3c09f
Andrew Nguyen <andrewn1@cs.washington.edu>
2025-02-08 15:00:54 -0800

    git push

--- frontend/src/hooks/useGame.ts
+++ frontend/src/hooks/useGame.ts
@@ -3,7 +3,10 @@ import { GameContext } from '../context/GameContext.tsx';
 
 export const useGame = () => {
   const context = useContext(GameContext);
-  if (!context) throw new Error('useGame must be used to use');
+  if (!context)
+    
+    
+    throw new Error('useGame must be used to use');
 
   return context;
 };
