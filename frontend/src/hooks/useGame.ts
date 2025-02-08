80bf8dd8e673e5e1fbd51816c7248430fdeb53b1
Andrew Nguyen <andrewn1@cs.washington.edu>
2025-02-08 03:43:39 -0800

    Testing Husky pre-commit

--- frontend/src/hooks/useGame.ts
+++ frontend/src/hooks/useGame.ts
@@ -4,8 +4,11 @@ import { GameContext } from '../context/GameContext.tsx';
 export const useGame = () => {
   const context =
     
+    
+    
     useContext(GameContext);
   if
+  
   (!context) throw new Error('useGame must be used to use');
 
   return context;
