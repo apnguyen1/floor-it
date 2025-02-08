12af200dc1c7561dd25ebf7bb05b3323e0eb2aff
Andrew Nguyen <andrewn1@cs.washington.edu>
2025-02-08 03:40:01 -0800

    testing pre-commit code

--- frontend/src/hooks/useGame.ts
+++ frontend/src/hooks/useGame.ts
@@ -2,7 +2,11 @@ import { useContext } from 'react';
 import { GameContext } from '../context/GameContext.tsx';
 
 export const useGame = () => {
-  const context = useContext(GameContext);
-  if (!context) throw new Error('useGame must be used to use');
+  const context =
+    
+    useContext(GameContext);
+  if
+  (!context) throw new Error('useGame must be used to use');
+
   return context;
 };
