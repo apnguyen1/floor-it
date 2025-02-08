b5342d6cacfd0d9e082c872f610bdb16fb13d7f9
Andrew Nguyen <andrewn1@cs.washington.edu>
2025-02-08 15:18:13 -0800

    tidy up

--- backend/src/scripts/brand_icons.py
+++ backend/src/scripts/brand_icons.py
@@ -13,11 +13,3 @@ class BrandIcons(Category[BrandIconsDTO]):
 
     def _format_data(self) -> dict[str, List[str]]:
         return self.brand_logo_to_name()
-
-
-if __name__ == "__main__":
-    bi = BrandIcons(
-        "brand-icons.json",
-    )
-
-    print(bi._formatted_data)
