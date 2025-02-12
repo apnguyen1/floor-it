1e799160d085791130e3b39bfe59415dfbe141e0
LoonTonk <93897399+LoonTonk@users.noreply.github.com>
2025-02-11 19:31:47 -0800

    Modified champions class so it doesn't store question_type and qt simultaneously

--- backend/src/scripts/lol_champions.py
+++ backend/src/scripts/lol_champions.py
@@ -8,7 +8,6 @@ from backend.src.utils.category import Category
 class Champions(Category[ChampionDTO]):
     def __init__(self, qt: QuestionType):
         url = "https://ddragon.leagueoflegends.com/cdn/15.2.1/data/en_US/champion.json"
-        self.qt = qt
         super().__init__(source=url, model=ChampionDTO, question_type=qt)
 
     def __title_to_name(self) -> Dict[str, List[str]]:
@@ -40,6 +39,6 @@ class Champions(Category[ChampionDTO]):
     def _format_data(self) -> dict[str, List[str]]:
         return (
             self.__title_to_name()
-            if self.qt == QuestionType.TEXT
+            if self.question_type == QuestionType.TEXT
             else (self.__image_to_name())
         )
