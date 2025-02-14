61ae49934439967c59b94fa47e523270112d2b14
Andrew Nguyen <59133789+apnguyen1@users.noreply.github.com>
2025-02-13 21:40:39 -0800

    (CI): Setting up Vitest  (#80)
    * vitest added
    
    * testing new pre-commit
    
    * testing new CI actions
    
    ---------
    
    Co-authored-by: Andrew Nguyen <andrewn1@cs.washington.edu>

---
+++ frontend/src/screens/Home/HomeScreen.test.tsx
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest';
+
+// TODO: To be removed after homepage is implemented.
+describe('HomeScreen', () => {
+  it('renders without crashing', () => {
+    expect(1 + 2).toBe(3);
+  });
+});
