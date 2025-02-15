c76088faef7b80269f6461d3746f32b098ce550d
Andrew Nguyen <andrewn1@cs.washington.edu>
2025-02-15 02:07:17 -0800

    setting up for future tickets

--- frontend/src/components/Player.tsx
+++ frontend/src/components/Player.tsx
@@ -1,61 +1,23 @@
 import { Avatar, Box } from '@mui/material';
-import { deepOrange } from '@mui/material/colors';
 import React from 'react';
 import Timer from './Timer.tsx';
 
 interface PlayerProps {
   playerName: string;
-  initialTime: number;
+  isActive: boolean;
 }
 
-export const Player: React.FC<PlayerProps> = ({
-  playerName,
-  initialTime,
-}: PlayerProps) => {
+export const Player: React.FC<PlayerProps> = ({ playerName }: PlayerProps) => {
   const fullName: string[] = playerName.split(' ');
   const abbrev: string = fullName.map((s) => s.charAt(0).toUpperCase()).join('');
 
   return (
     <Box
-      className="player-frame-box"
-      sx={{
-        flex: 2,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '75%',
-        width: '100%',
-      }}
+      sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', flex: 1 }}
     >
-      <Box
-        className="player-content-box"
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          height: '50%',
-          alignItems: 'center',
-
-          justifyContent: 'space-around',
-          gap: 1,
-        }}
-      >
-        <Box className="player-avatar-box">
-          <Avatar
-            sx={{
-              bgcolor: deepOrange[500],
-              width: 64,
-              height: 64,
-              fontSize: 24,
-            }}
-          >
-            {abbrev}
-          </Avatar>
-        </Box>
-        <Timer initialTime={initialTime} />
-        <Box className="player-turn-box" sx={{ fontSize: '1rem', fontWeight: 'bold' }}>
-          {fullName[0]}'s turn
-        </Box>
-      </Box>
+      <Avatar>{abbrev}</Avatar>
+      <Timer />
+      <h3>{fullName[0]}'s Turn</h3>
     </Box>
   );
 };
--- frontend/src/components/Timer.tsx
+++ frontend/src/components/Timer.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 
-interface TimerProps {
-  initialTime: number;
-}
-
 // TODO: To be implemented ticket-15
-const Timer: React.FC<TimerProps> = ({ initialTime }: TimerProps) => {
+const Timer: React.FC = () => {
   const formatTime = (time: number) => {
     const seconds = Math.floor(time / 1000);
     const milliseconds = Math.floor((time % 1000) / 10);
     return `${seconds}.${milliseconds.toString().padStart(2, '0')}`;
   };
 
-  return <h2>{formatTime(initialTime)}</h2>;
+  return <h2>{formatTime(450000)}</h2>;
 };
 
 export default Timer;
