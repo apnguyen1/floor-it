b0354465a1bee8b6fffabc2876f21725fe75e85c
Brian S. Yu <gobrianyu@gmail.com>
2025-02-15 17:24:20 -0800

    Added fetch.ts api to get category list and data (#85)
    * Added fetch.ts api to get category list and data
    
    * Fixing linting issues with Prettier --write
    
    * Updating config files to include node types
    
    * removed tests and improved fetch.ts implementation
    
    ---------
    
    Co-authored-by: brian y. <brian127@uw.edu>
    Co-authored-by: Andrew Nguyen <59133789+apnguyen1@users.noreply.github.com>

---
+++ frontend/src/types/category.ts
@@ -0,0 +1,23 @@
+export interface CategoryPreview {
+  name: string;
+  image: string;
+  desc: string;
+}
+
+export interface CategoryList {
+  categories: CategoryPreview[];
+}
+
+export interface Question {
+  question: string;
+  answers: string[];
+  aliases: string[];
+}
+
+export interface CategoryContent {
+  name: string;
+  preview_img: string;
+  preview_desc: string;
+  type: 'text' | 'img';
+  questions: Question[];
+}
