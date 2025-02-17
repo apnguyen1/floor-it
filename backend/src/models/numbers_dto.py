867240fc32cfeb9e4211897ec08eaf3c79046dda
aafsoun <aafsoun@cs.washington.edu>
2025-02-16 17:37:19 -0800

    Test

--- backend/src/models/numbers_dto.py
+++ backend/src/models/numbers_dto.py
@@ -1,7 +1,6 @@
 from typing import List
 from pydantic import Field
 from backend.src.models.model import Model
-
 class NumberDTO(Model):
     question: str = Field(alias="Question")
     answers: List[str] = Field(alias="Answers")
