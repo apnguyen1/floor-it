26ec632ca33c7c0ced38c494a1e9a68678c09830
Andrew Nguyen <andrewnguyen17678@gmail.com>
2025-03-06 16:14:54 -0800

    Ease of adding preview images

--- backend/src/utils/definitions.py
+++ backend/src/utils/definitions.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent.parent.parent.parent
@@ -7,3 +8,10 @@ RESOURCE_DIR = ROOT_DIR / "backend/src/resources"
 SCRIPTS_DIR = ROOT_DIR / "backend/src/scripts"
 
 PUBLIC_DIR = ROOT_DIR / "frontend/public"
+
+
+class PreviewImageType(str, Enum):
+    DEFAULT = "default-preview.png"
+    GAMES = "games_icon.png"
+    NUMBERS = "numbers.png"
+    SHOPPING = "shopping_bag.png"
